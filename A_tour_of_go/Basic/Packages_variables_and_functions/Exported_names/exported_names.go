26cdaa84432af654d13bac1e3a1992abf18f63e7
Richard David <rjdgonzalez.contractor@bbva.com>
2017-03-15 14:26:23 -0300

    Eliminación de espacio en archivo exported_names.

--- A_tour_of_go/Basic/Packages_variables_and_functions/Exported_names/exported_names.go
+++ A_tour_of_go/Basic/Packages_variables_and_functions/Exported_names/exported_names.go
@@ -8,7 +8,6 @@ import (
 const pi float32 = math.Pi
 
 func main() {
-
 	//Pi is exported name and pi don't is exported name. This is exported the package math.
 	fmt.Println("This is the number PI exported of the package math:", pi)
 }
