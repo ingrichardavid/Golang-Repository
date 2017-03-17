f357c517dfdec4558a5ae667daa9d008a27e7377
Richard David <rjdgonzalez.contractor@bbva.com>
2017-03-17 15:15:59 -0300

    Se añadió un ejemplo sobre el uso de declaración corta de variables.

---
+++ A_tour_of_go/Basic/Packages_variables_and_functions/10. Short_variable_declarations/short_variable_declarations.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var k, j int = 3, 4
+
+func main() {
+	m := 5
+	java, objetiveC, swift, golang := true, false, true, "Sí!"
+	fmt.Println(k, j, m, java, objetiveC, swift, golang)
+}
