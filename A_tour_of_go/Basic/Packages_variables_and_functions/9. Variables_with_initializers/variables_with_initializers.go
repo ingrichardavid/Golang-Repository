9b3ac060d7d4ac61bf4c8065a8e747b46f61f9c4
Richard David <rjdgonzalez.contractor@bbva.com>
2017-03-17 14:19:36 -0300

    Se añadió ejemplo sobre el uso de variables con inicializadores.

---
+++ A_tour_of_go/Basic/Packages_variables_and_functions/9. Variables_with_initializers/variables_with_initializers.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var k, j int = 1, 2
+
+func main() {
+	var java, objetiveC, swift, golang = true, false, true, "Sí!"
+	fmt.Println(k, j, java, objetiveC, swift, golang)
+}
